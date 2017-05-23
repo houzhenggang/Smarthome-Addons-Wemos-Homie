256fbbc051c31893c9aa55b1f6852ad2c5df8166
Holger Imbery <holger@imbery.de>
2017-05-23 17:50:21 +0200

    c9 OTA added

--- SonoffTouch/src/relaynode.ino
+++ SonoffTouch/src/relaynode.ino
@@ -23,6 +23,9 @@
   SOFTWARE.
 */
 #include <Homie.h>
+//
+#include <ArduinoOTA.h>
+//
 
 #define PIN_RELAY 12
 #define PIN_LED 13
@@ -69,7 +72,7 @@ void setup() {
   pinMode(PIN_RELAY, OUTPUT);
   digitalWrite(PIN_RELAY, LOW);
 
-  Homie_setFirmware("SonoffTouch", "1.1.2");
+  Homie_setFirmware("SonoffTouch", "1.1.3ota");
   Homie.setLedPin(PIN_LED, LOW).setResetTrigger(PIN_BUTTON, LOW, 5000);
 
   Homie.setSetupFunction(setupHandler);
@@ -79,10 +82,55 @@ void setup() {
   #ifdef CLEAN_UP
     Homie.reset();
   #endif
+
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+
   Homie.setup();
 }
 
 void loop() {
+  ArduinoOTA.handle();
   int reading = digitalRead(PIN_BUTTON);
   if (reading != lastButtonState) {
     lastDebounceTime = millis();
